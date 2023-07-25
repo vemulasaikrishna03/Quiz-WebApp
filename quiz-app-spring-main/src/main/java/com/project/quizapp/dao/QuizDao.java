655fbf6098b2b0d2895d3a38566ad99ead7a8bce
Sai Krishna <76392114+vemulasaikrishna03@users.noreply.github.com>
2023-07-25 20:59:52 +0530

    Delete quiz-app-spring-main directory

--- quiz-app-spring-main/src/main/java/com/project/quizapp/dao/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.telusko.quizapp.dao;
-
-import com.telusko.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz,Integer> {
-}
